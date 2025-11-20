2e19ccf98e424227bf5ab7f781bc36100e361e90
MdAsraful56 <work.mdasraful56@gmail.com>
2025-11-20 08:46:54 +0600

    porxy.ts file update

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,15 +1,94 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
+type UserRole = 'ADMIN' | 'DOCTOR' | 'PATIENT';
+
+type RouteConfig = {
+    exact: string[];
+    patterns: RegExp[];
+};
+
+const authRoutes = ['/login', '/register', '/forgot-password'];
+
+const commonProtectedRoutes: RouteConfig = {
+    exact: ['/profile', '/settings'],
+    patterns: [],
+};
+
+const doctorProtectedRoutes: RouteConfig = {
+    patterns: [/^\/doctor\/.*/],
+    exact: [],
+};
+
+const patientProtectedRoutes: RouteConfig = {
+    patterns: [/^\/patient\/.*/],
+    exact: [],
+};
+
+const adminProtectedRoutes: RouteConfig = {
+    patterns: [/^\/admin\/.*/],
+    exact: [],
+};
+
+const isAuthRoute = (pathName: string) => {
+    return authRoutes.some((route: string) => route === pathName);
+};
+
+const isRouteMatches = (pathName: string, routes: RouteConfig) => {
+    if (routes.exact.includes(pathName)) {
+        return true;
+    }
+
+    return routes.patterns.some((pattern: RegExp) => pattern.test(pathName));
+};
+
+const getRouteOwner = (
+    pathName: string
+): 'ADMIN' | 'DOCTOR' | 'PATIENT' | 'COMMON' | null => {
+    if (isRouteMatches(pathName, adminProtectedRoutes)) {
+        return 'ADMIN';
+    }
+    if (isRouteMatches(pathName, doctorProtectedRoutes)) {
+        return 'DOCTOR';
+    }
+    if (isRouteMatches(pathName, patientProtectedRoutes)) {
+        return 'PATIENT';
+    }
+    if (isRouteMatches(pathName, commonProtectedRoutes)) {
+        return 'COMMON';
+    }
+    return null;
+};
+
+const getDefaultDashboardRoute = (role: UserRole): string => {
+    switch (role) {
+        case 'ADMIN':
+            return '/admin/dashboard';
+        case 'DOCTOR':
+            return '/doctor/dashboard';
+        case 'PATIENT':
+            return '/patient/dashboard';
+        default:
+            return '/';
+    }
+};
+
 // This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
-    return NextResponse.redirect(new URL('/home', request.url));
+    console.log('pathName', request.nextUrl.pathname);
+    return NextResponse.next();
 }
-
-// Alternatively, you can use a default export:
-// export default function proxy(request: NextRequest) { ... }
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: '/about/:path*',
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+         */
+        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+    ],
 };
